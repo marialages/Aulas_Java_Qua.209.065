192125a77f8740ddd08d01e557c79924fd79a62b
marialagess <maria.lages1895@gmail.com>
2023-06-19 21:17:40 -0300

    19.06.2023

---
+++ AtividaDeHeranca/src/principal/Program.java
@@ -0,0 +1,27 @@
+package principal;
+
+import entities.Funcionario;
+import entities.Gerente;
+import entities.Secretaria;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Funcionario funcionario = new Funcionario("Jose","financeiro",'M', 50 , 5000);
+		Funcionario gerente = new Gerente("Janaina","gerente",'F', 20 , 7500);
+		Funcionario secretaria = new Secretaria("Leone", "Secretario escolar",'M', 23, 1200);
+		
+		funcionario.bonificacao();
+		gerente.bonificacao();
+		secretaria.bonificacao();
+
+		System.out.println(funcionario);
+		System.out.println(gerente);
+		System.out.println(secretaria);
+
+		
+	}
+
+}
