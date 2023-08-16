f299cd31b70519a9f40dd5e705bdfe4675e60ed0
marialagess <maria.lages1895@gmail.com>
2023-08-15 21:22:14 -0300

    projeto

---
+++ crudComSpring/src/main/java/br/com/model/Produto.java
@@ -0,0 +1,16 @@
+package br.com.model;
+
+public class Produto {
+
+	private Long id;
+	
+	private String nome;
+	
+	private int quantidade;
+	
+	private double preco;
+	
+	private String descricaoProduto;
+	
+	private String urlImagem;
+}
